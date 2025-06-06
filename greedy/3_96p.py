6c7f694bb1098afae33e43c846192bc62b147a15
grey0720 <grey0720@gmail.com>
2025-06-06 21:41:06 +0900

    그리디 알고리즘

---
+++ greedy/3_96p.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+card = []
+mx = 0
+answer = 0
+
+for i in range(n):
+    l = list(map(int, input().split()))
+    if mx < min(l):
+        answer = i
+        mx = min(l)
+
+print(mx)
